56f88695613b49df1ff9b4075b743463f7c2d21c
Thomas Bell <thomasjbell065@gmail.com>
2022-05-10 13:26:49 -0400

    updated app page
    added some code to finish the app portion, used arrays and functions, copy/pasted code over to "The Code"

--- js/site.js
+++ js/site.js
@@ -1,4 +1,36 @@
-//Call Hello World
-function helloWorld() {
-    alert("hello World");
+//Get the string from the page
+//controller function
+function getValues() {
+
+    document.getElementById("alert").classList.add("invisible");
+    
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+//Reverse the string
+//logic function
+function reverseString(userString) {
+
+    let revString = [];
+
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+
+    }
+    return revString;
+}
+
+//Display the reversed string in a message to the user
+//viewfunction
+function displayString(revString) {
+
+    //write message to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
